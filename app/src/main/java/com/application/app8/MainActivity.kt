a96fbcf9eb57369ae8ae6dd64bff86c69c74f13a
Shivesh Kumar <kshivesh99@gmail.com>
2021-08-31 03:12:25 +0530

    Added ViewModel,RoomDatabase

--- app/src/main/java/com/application/app8/MainActivity.kt
+++ app/src/main/java/com/application/app8/MainActivity.kt
@@ -6,38 +6,60 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var todoList= mutableListOf<ToDo>(
-        ToDo("Follow me on Instagram", false),
-        ToDo("Learn about recyclerView", false),
-        ToDo("Feed my cat", false),
-        ToDo("Prank my BOSS", false),
-        ToDo("Eat some curry", false),
-        ToDo("Ask my crush out", false),
-        ToDo("Take a shower", false),
-        ToDo("You can add an item now", false),
-        ToDo("Also you can delete from the list", false),
-        ToDo("But the app is still in progress", false),
-        ToDo("Hopefully we would complete this", false),
-        ToDo("that's why so much of dummy data", false)
-    )
-    val adapter=ToDoAdapter(todoList)
+
+//    var todoList= mutableListOf<ToDo>(
+//        ToDo("Follow me on Instagram", false),
+//        ToDo("Learn about recyclerView", false),
+//        ToDo("Feed my cat", false),
+//        ToDo("Prank my BOSS", false),
+//        ToDo("Eat some curry", false),
+//        ToDo("Ask my crush out", false),
+//        ToDo("Take a shower", false),
+//        ToDo("You can add an item now", false),
+//        ToDo("Also you can delete from the list", false),
+//        ToDo("But the app is still in progress", false),
+//        ToDo("Hopefully we would complete this", false),
+//        ToDo("that's why so much of dummy data", false)
+//    )
+//    val adapter=ToDoAdapter(todoList)
+    lateinit var mainViewModel: NoteViewModel
+    var todoCount=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        rvToDos.adapter=adapter
-        rvToDos.layoutManager=LinearLayoutManager(this)
+        //
+        rvToDos.layoutManager = LinearLayoutManager(this)
+        val adapter = ToDoAdapter(this, this)
+        rvToDos.adapter = adapter
+        //
+
+
+        mainViewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        mainViewModel.allNotes.observe(this,
+            Observer {list->
+            list?.let{
+                adapter.updateList(it)
+            }})
+
+
+            //rvToDos.adapter=adapter
+        //rvToDos.layoutManager=LinearLayoutManager(this)
         val deleteAlert=AlertDialog.Builder(this)
             .setTitle("Delete")
             .setMessage("Do you want to delete?")
             .setIcon(R.drawable.ic_delete)
             .setPositiveButton("Yes"){ _, _ ->
-                deleteItem()
+//                Toast.makeText(this,"Feature Not Added",Toast.LENGTH_SHORT).show()
+                    deleteItem()
             }
             .setNegativeButton("No"){_, _ ->
 
@@ -54,11 +76,19 @@ class MainActivity : AppCompatActivity() {
                 }
                 else{
                     val title=etTodo.text.toString()
-                    val todo=ToDo(title,false)
-                    todoList.add(todo)
+
+                    //
+                    val todo=Note(title,checkBox=0)
+                    todoCount+=1
+                    mainViewModel.insertNote(todo)
+                    //
+
+
+                    //val todo=ToDo(title,false)
+                    //todoList.add(todo)
                     etTodo.text=null
-                    adapter.notifyItemInserted(todoList.size-1)
-                    Log.d("insertItem","Insert a item")
+//                    adapter.notifyItemInserted(todoList.size-1)
+//                    Log.d("insertItem","Insert a item")
                 }
             }
             .setNegativeButton("No"){_, _ ->
@@ -74,22 +104,27 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun deleteItem() {
-        while (true) {
-            var letDelete = -1
-            for (i in todoList.indices) {
-                if (todoList[i].isChecked) {
-                    letDelete = i
-                    break
-                }
-            }
-            if (letDelete == -1) {
-                break
-            } else {
-                todoList.removeAt(letDelete)
-                Log.d("shivesh", "$letDelete")
-                adapter.notifyItemRemoved(letDelete)
-            }
-        }
-    }
+
+
+}
+private fun deleteItem() {
+    //mainViewModel.deleteNote()
+
+//        while (true) {
+//            var letDelete = -1
+//            for (i in todoList.indices) {
+//                if (todoList[i].isChecked) {
+//                    letDelete = i
+//                    break
+//                }
+//            }
+//            if (letDelete == -1) {
+//                break
+//            } else {
+//                todoList.removeAt(letDelete)
+//                Log.d("shivesh", "$letDelete")
+//                adapter.notifyItemRemoved(letDelete)
+//            }
+//        }
+}
 }
--- app/src/main/java/com/application/app8/ToDoAdapter.kt
+++ app/src/main/java/com/application/app8/ToDoAdapter.kt
@@ -1,23 +1,52 @@
 package com.application.app8
 
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_todo.view.*
 
 class ToDoAdapter(
-    var todos:List<ToDo>
+
+
+    //
+    private val context: Context,
+    private val listener: TodoCLickListener
+    //
+
+
+    //var todos: ArrayList<Note>
 ): RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder>(){
+
+    //
+    var todos: ArrayList<Note>()
+    //
+
+
     inner class ToDoViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
-        var title = itemView.tvTitle
-        var isChecked = itemView.cbDone
+        var title:TextView = itemView.tvTitle
+        var isChecked:CheckBox = itemView.cbDone
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_todo,parent,false)
-        return ToDoViewHolder(view)
+        //val view=LayoutInflater.from(parent.context).inflate(R.layout.item_todo,parent,false)
+
+
+        //
+        var view=ToDoViewHolder(LayoutInflater.from(context).inflate(R.layout.item_todo,parent,false))
+        //
+
+        view.checkBox.setOnClickListener {
+            val position = view.adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClicked(todos[position])
+            }
+        }
+        return view
     }
 
     override fun getItemCount(): Int {
@@ -26,24 +55,31 @@ class ToDoAdapter(
 
     override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
 
-        holder.title.text = todos[position].title
-        holder.isChecked.isChecked = todos[position].isChecked
+        holder.title.text = todos[position].text
 
-        holder.isChecked.setOnClickListener {
-            if(position != RecyclerView.NO_POSITION) {
-                todos[position].isChecked = !todos[position].isChecked
-                notifyDataSetChanged()
-            }
-        }
+//        holder.isChecked.setOnClickListener {
+//            if(position != RecyclerView.NO_POSITION) {
+//
+//                notifyDataSetChanged()
+//            }
+//        }
 
-//        holder.itemView.apply {
-//            tvTitle.text=todos[position].title
-//            cbDone.isChecked=todos[position].isChecked
-//        }
-//        holder.itemView.cbDone.setOnClickListener {
-//            holder.itemView.cbDone.isChecked != holder.itemView.cbDone.isChecked
-//            todos[position].isChecked != todos[position].isChecked
-//        }
+
+        //
+        var isChecked = false
+        if(todos[position].checkBox == 1) isChecked = true
+        holder.checkBox.isChecked = isChecked
+        //
 
     }
+
+    fun updateList(newList: List <Note>) {
+        todos.clear()
+        todos.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+}
+interface TodoCLickListener {
+    fun onItemClicked(note: Note)
 }
